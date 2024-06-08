ce2f39d5750a4827926ebfe5b8d760b86b13caf4
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-06-08 20:16:27 +0200

    Incorporating Kafka Configuration and producer essential configs.

---
+++ services/order-service/src/main/java/com/rca/ecommerce/config/KafkaOrderTopicConfig.java
@@ -0,0 +1,14 @@
+package com.rca.ecommerce.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaOrderTopicConfig {
+    @Bean
+    public NewTopic orderTopic() {
+        return TopicBuilder.name("order-topic").build();
+    }
+}
